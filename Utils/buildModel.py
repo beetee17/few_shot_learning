192ffd241e3253c19437a112450c598cca5c2cdd
beetee17 <brandon.thio@yahoo.com.sg>
2021-05-03 16:14:21 +0800

    with more options + resnet

--- Utils/buildModel.py
+++ Utils/buildModel.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from keras.models import Sequential, Model
 
 from keras.applications.vgg16 import VGG16
+from keras.applications.resnet import ResNet50
  
 from keras.layers import Flatten
 from keras.layers import Flatten, Conv2D, MaxPooling2D, Dense, Concatenate, Dot, Lambda, Input, Dropout, BatchNormalization
@@ -88,7 +89,7 @@ def get_feature_extractor(input_shape):
     return model
 
 
-def get_pretrained_model(input_shape, num_dense=1, dense_size=(256)):
+def get_pretrained_model(input_shape, num_dense=1, dense_size=(256), base='vgg16', trainable=1, batch_norm=True, dropout=0):
     """
         Model architecture
     """
@@ -99,34 +100,43 @@ def get_pretrained_model(input_shape, num_dense=1, dense_size=(256)):
 
     # Convolutional Neural Network
     pre_train = Sequential()
-    base = VGG16(include_top=False, input_shape=input_shape)
+        
+    if base == 'resnet':
+        base = ResNet50(include_top=False, input_shape=input_shape, weights='imagenet')
+        pre_train.add(base)
+
+        for layer in base.layers[:-trainable]:
+            layer.trainable = False
+            print(layer.name, layer.trainable)
+
+        for layer in base.layers[-trainable:]:
+            layer.trainable = True
+            print(layer.name, layer.trainable)
+        
+
+    elif base == 'vgg16':
+        base = VGG16(include_top=False, input_shape=input_shape)
   
-    # freeze all layers but last 4
-    for layer in base.layers[:-4]:
-        layer.trainable = False
-        pre_train.add(layer)
+        # freeze all layers but last few, specified by trainable kwarg
+        for layer in base.layers[:-trainable]:
+            layer.trainable = False
+            pre_train.add(layer)
 
-    for layer in base.layers[-4:]:
-        pre_train.add(BatchNormalization())
-        pre_train.add(layer)
+        for layer in base.layers[-trainable:]:
+            # pre_train.add(BatchNormalization())
+            layer.trainable = True
+            pre_train.add(layer)
 
-    # for layer in base.layers:
-    #     print(layer.name, layer.trainable)
-    
-    # pre_train.add(base)
+
     pre_train.add(tf.keras.layers.Flatten())
 
     for i in range(num_dense):
-        pre_train.add(BatchNormalization())
+        if batch_norm:
+            pre_train.add(BatchNormalization())
         pre_train.add(Dense(dense_size[i]))
         # pre_train.add(Dense(dense_size[i], kernel_constraint=constraints.max_norm(2.)))
-        # pre_train.add(Dropout(0.5))
-    
-    # pre_train.pop()
-    
-    for layer in pre_train.layers:
-        print(layer.name, layer.trainable)
-
+        if dropout > 0:
+            pre_train.add(Dropout(dropout))
 
     embedding = Model(pre_train.input, pre_train.output, name="Embedding")
     
@@ -147,67 +157,5 @@ def get_pretrained_model(input_shape, num_dense=1, dense_size=(256)):
     pretrained_model = Model(inputs=[left_input,right_input], outputs=prediction)
 
     # return the model
-    return pretrained_model
-
-class EarlyStoppingAtMinLoss(keras.callbacks.Callback):
-    """Stop training when the loss is at its min, i.e. the loss stops decreasing.
-
-  Arguments:
-      patience: Number of epochs to wait after min has been hit. After this
-      number of no improvement, training stops.
-  """
-
-    def __init__(self, val_inputs, val_labels, patience=0, batch_size=16):
-        super(EarlyStoppingAtMinLoss, self).__init__()
-        self.patience = patience
-        # best_weights to store the weights at which the minimum loss occurs.
-        self.best_weights = None
-        self.best_epoch = 0
-        self.val_inputs = val_inputs
-        self.val_labels = val_labels
-        self.batch_size = batch_size
-
-    def on_train_begin(self, logs=None):
-        # The number of epoch it has waited when loss is no longer minimum.
-        self.wait = 0
-        # The epoch the training stops at.
-        self.stopped_epoch = 0
-        # Initialize the best as infinity.
-        self.best = np.Inf
-
-    def on_epoch_begin(self, epoch, logs=None):
-        if epoch != 0:
-            print('\n\n')
-
-    def on_epoch_end(self, epoch, logs=None):
-        
-        print('\nEvaluating model on validations set')
-        metrics = self.model.evaluate(x=self.val_inputs, y=self.val_labels, batch_size=self.batch_size, verbose=1, sample_weight=None, steps=None, return_dict = True)
-        current = metrics['loss']
-
-        # for plotting learning process
-      
-        logs['val_loss'] = metrics['loss']
-        logs['val_acc'] = metrics['accuracy']
-
-        if np.less(current, self.best):
-            self.best = current
-            self.wait = 0
-            self.best_epoch = epoch + 1
-            # Record the best weights if current results is better (less).
-            self.best_weights = self.model.get_weights()
-        else:
-            self.wait += 1
-            if self.wait >= self.patience:
-                self.stopped_epoch = epoch + 1
-                self.model.stop_training = True
-                print("Restoring model weights from the end of the best epoch - epoch {}.".format(self.best_epoch))
-                self.model.set_weights(self.best_weights)
-
-    def on_train_end(self, logs=None):
-        if self.stopped_epoch > 0:
-            print("Epoch {}: early stopping".format(self.stopped_epoch))
-        else:
-            self.model.set_weights(self.best_weights)
-            print("set weights from epoch {}".format(self.best_epoch))
-        
+    return pretrained_model 
+    
