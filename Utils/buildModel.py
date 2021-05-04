680f1055ec8770ece611b70839745bec3b1933b7
beetee17 <brandon.thio@yahoo.com.sg>
2021-05-04 17:02:46 +0800

    fixed save/load issue due to flatten layer

--- Utils/buildModel.py
+++ Utils/buildModel.py
@@ -13,7 +13,6 @@ from tensorflow.keras import initializers
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras import constraints
 
-from keras.optimizers import Adam
 from keras import backend as K
 
 import numpy as np
@@ -74,10 +73,16 @@ def get_feature_extractor(input_shape):
     left_input = Input(input_shape)
     right_input = Input(input_shape)
 
-    base = VGG16(include_top=False, input_shape=input_shape)
-    base.add(tf.keras.layers.Flatten())
+    base = Sequential()
+    vgg16 = VGG16(include_top=False, input_shape=input_shape)
 
-    extractor = Model(base.input, base.output, name="Feature Extractor")
+    base.add(vgg16)
+
+    flatten = Lambda(lambda v: K.reshape(v, (-1, np.prod(np.array(v.shape[1:])))))
+
+    base.add(flatten)
+
+    extractor = Model(base.input, base.output)
 
     encoded_l = extractor(left_input)
     encoded_r = extractor(right_input)
@@ -86,14 +91,14 @@ def get_feature_extractor(input_shape):
 
     model = Model(inputs=[left_input,right_input], outputs=cos_sim)
 
-    return model
+    return model    
 
 
-def get_pretrained_model(input_shape, num_dense=1, dense_size=(256), base='vgg16', trainable=1, batch_norm=True, dropout=0):
+def get_pretrained_model(input_shape, num_dense=1, dense_size=(256,), base='vgg16', trainable=4, batch_norm=True, dropout=0):
     """
         Model architecture
     """
-
+    # return Pretrained(input_shape, num_dense, dense_size, base, trainable, batch_norm, dropout)
     # Define the tensors for the two input images
     left_input = Input(input_shape)
     right_input = Input(input_shape)
@@ -121,6 +126,7 @@ def get_pretrained_model(input_shape, num_dense=1, dense_size=(256), base='vgg16
         for layer in base.layers[:-trainable]:
             layer.trainable = False
             pre_train.add(layer)
+            
 
         for layer in base.layers[-trainable:]:
             if batch_norm:
@@ -129,16 +135,19 @@ def get_pretrained_model(input_shape, num_dense=1, dense_size=(256), base='vgg16
             pre_train.add(layer)
 
 
-    pre_train.add(tf.keras.layers.Flatten())
+    pre_train.add(Lambda(lambda v: K.reshape(v, (-1, np.prod(np.array(v.shape[1:]))))))
 
     for i in range(num_dense):
         if batch_norm:
             pre_train.add(BatchNormalization())
         pre_train.add(Dense(dense_size[i]))
-        # pre_train.add(Dense(dense_size[i], kernel_constraint=constraints.max_norm(2.)))
+
         if dropout > 0:
             pre_train.add(Dropout(dropout))
 
+    for layer in pre_train.layers:
+        print(layer.name, layer.trainable)
+        
     embedding = Model(pre_train.input, pre_train.output, name="Embedding")
     
 
@@ -152,11 +161,74 @@ def get_pretrained_model(input_shape, num_dense=1, dense_size=(256), base='vgg16
     # Add a dense layer with a sigmoid unit to generate the similarity score
     prediction = Dense(1,activation='sigmoid',kernel_initializer=initializers.Constant(value=5), bias_initializer='zeros')(cos_sim)
 
-    # prediction = Lambda(lambda x: 1-x)(tanh)
-
     # # Connect the inputs with the outputs
-    pretrained_model = Model(inputs=[left_input,right_input], outputs=prediction)
+    pretrained_model = Model(inputs=[left_input,right_input], outputs=prediction, name='model')
 
     # return the model
     return pretrained_model 
 
+class EarlyStoppingAtMinLoss(keras.callbacks.Callback):
+    """Stop training when the loss is at its min, i.e. the loss stops decreasing.
+  Arguments:
+      patience: Number of epochs to wait after min has been hit. After this
+      number of no improvement, training stops.
+  """
+
+    def __init__(self, val_inputs, val_labels, patience=0, batch_size=16):
+        super(EarlyStoppingAtMinLoss, self).__init__()
+        self.patience = patience
+        # best_weights to store the weights at which the minimum loss occurs.
+        self.best_weights = None
+        self.best_epoch = 0
+        self.val_inputs = val_inputs
+        self.val_labels = val_labels
+        self.batch_size = batch_size
+
+    def on_train_begin(self, logs=None):
+        # The number of epoch it has waited when loss is no longer minimum.
+        self.wait = 0
+        # The epoch the training stops at.
+        self.stopped_epoch = 0
+        # Initialize the best as infinity.
+        self.best = np.Inf
+
+    def on_epoch_begin(self, epoch, logs=None):
+        if epoch != 0:
+            print('\n\n')
+
+    def on_epoch_end(self, epoch, logs=None):
+
+        print('\nEvaluating model on validations set')
+        metrics = self.model.evaluate(x=self.val_inputs, y=self.val_labels, batch_size=self.batch_size, verbose=1, sample_weight=None, steps=None, return_dict = True)
+        current = metrics['loss']
+
+        # for plotting learning process
+
+        logs['val_loss'] = metrics['loss']
+        logs['val_acc'] = metrics['accuracy']
+
+        if np.less(current, self.best):
+            self.best = current
+            self.wait = 0
+            self.best_epoch = epoch + 1
+            # Record the best weights if current results is better (less).
+            self.best_weights = self.model.get_weights()
+        else:
+            self.wait += 1
+            if self.wait >= self.patience:
+                self.stopped_epoch = epoch + 1
+                self.model.stop_training = True
+                print("Restoring model weights from the end of the best epoch - epoch {}.".format(self.best_epoch))
+                self.model.set_weights(self.best_weights)
+
+    def on_train_end(self, logs=None):
+        if self.stopped_epoch > 0:
+            print("Epoch {}: early stopping".format(self.stopped_epoch))
+        else:
+            self.model.set_weights(self.best_weights)
+            print("set weights from epoch {}".format(self.best_epoch))
+
+if __name__ =='__main__':
+    model = get_pretrained_model((200, 280,3), num_dense=2, dense_size=(1024, 512))
+    model.summary()
+ 
