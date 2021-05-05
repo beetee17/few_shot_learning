6d5ba21753c6f2906db09b71cc98f7d5e62abe03
beetee17 <brandon.thio@yahoo.com.sg>
2021-05-05 16:02:12 +0800

    init, unable to display images on secondary page

--- aircraft/main.py
+++ aircraft/main.py
@@ -1,12 +1,16 @@
 import sys
 from typing import Optional
 
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, Request, File, UploadFile
+from fastapi.responses import Response
+from fastapi.templating import Jinja2Templates
 
 sys.path.append('c:\\Users\\Admin\\few_shot_learning\\')
 
 from Utils import preprocess, testModels,  utils
 
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 
@@ -23,7 +27,8 @@ model = keras.models.load_model(r'C:\Users\Admin\few_shot_learning\aircraft\mode
 def get_pairs(path):
 
     support_set = list()
-
+    
+ 
     query_img_dir = ['{}\query\{}'.format(path, i) for i in os.listdir(path + '\query')][0]
 
     support_imgs_dir = ['{}\support\{}'.format(path, i) for i in os.listdir(path + '\support')]
@@ -44,25 +49,66 @@ def get_pairs(path):
 
 
 app = FastAPI()
+db = []
 
+templates = Jinja2Templates(directory='templates')
+
+@app.post("/uploadfiles/")
+async def create_upload_files(request : Request, files: List[UploadFile] = File(...)):
+
+    for file in files:
+        contents = await file.read()
+
+        db.append(contents)
+
+    return {'filenames' : [file.filename for file in files]}
 
 @app.get("/")
-def read_root():
+def read_root(request : Request):
+    
+    return templates.TemplateResponse('home.html', {'request' : request,
+                                                    })
+    
 
-    return 'To get similarity scores between a query image and some support images, put them in a folder named "query" and "support" respectively. Then put the folders in another folder and pass the path to this root folder in the url like so:\nhttp://localhost:8000/get_predictions/?path=C:\\Users\\Admin\\few_shot_learning\\aircraft\\example'
 
-
-
+# http://localhost:8000/get_predictions/?path=path\\to\\folder
 @app.get("/get_predictions/")
-def get_predictions(path: str):
+def get_predictions(request : Request):
+    response = []
+    for f in db:
+        response.append(Response(content=f))
+    print(response)
+    return templates.TemplateResponse('predictions.html', {'request' : request, 'files' : response})
+    # if the dir exists in the current working directory
+    if os.path.isdir(os.getcwd() + '\\' + path):
 
+        path = os.getcwd() + '\\' + path
+        
+    # if the dir is not a full path and not in cwd, check if it exists
+    elif len(path.split('\\')) == 1:
+        for abs_path, directories, files in os.walk(r'C:\Users\Admin'):
+            
+            if path in directories:
+                path = os.path.join(abs_path, path)
+                print('found %s' % os.path.join(abs_path, path))
+
+    # the dir is not in cwd and was not found in local search -> it may be a full path
+            
     query_img_dir, support_imgs_dir, support_set = get_pairs(path)
     
-    predictions = model.predict([support_set[:,0], support_set[:,1]])
-    predictions = {support_imgs_dir[i] : str(predictions[i]) for i in range(len(support_imgs_dir))}
+    predictions = list(model.predict([support_set[:,0], support_set[:,1]]).flatten())
+    # predictions = {support_imgs_dir[i].replace(path + '\\support\\', '') : str(predictions[i]) for i in range(len(support_imgs_dir))}
 
 
     print(predictions)
 
+    
+    return templates.TemplateResponse('predictions.html', {'request' : request,
+                                                           'query_img_dir' : query_img_dir,
+                                                           'support_imgs_dir' : support_imgs_dir,
+                                                           'predictions' : predictions}) 
+
     return {"predictions": predictions}
+
+
 
